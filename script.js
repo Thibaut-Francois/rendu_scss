c2d776e51665bbc16892b9208b174ec17bf548d8
Vorn-Hagen <thibaut.francois@edu.devinci.fr>
2022-10-15 21:05:04 +0200

    :construction: Add all the html and css but img

---
+++ script.js
@@ -0,0 +1,39 @@
+document.getElementById("btn_top").addEventListener("click", function() {
+    document.getElementById('top').scrollIntoView({behavior: "smooth"})
+});
+
+document.getElementById("btn1").addEventListener("click", function() {
+    document.getElementById('section1').scrollIntoView({behavior: "smooth"})
+});
+
+document.getElementById("btn2").addEventListener("click", function() {
+    document.getElementById('section2').scrollIntoView({behavior: "smooth"})
+});
+
+document.getElementById("btn3").addEventListener("click", function() {
+    document.getElementById('section3').scrollIntoView({behavior: "smooth"})
+});
+
+document.getElementById("btn4").addEventListener("click", function() {
+    document.getElementById('section4').scrollIntoView({behavior: "smooth"})
+});
+
+document.getElementById("btn5").addEventListener("click", function() {
+    document.getElementById('section5').scrollIntoView({behavior: "smooth"})
+});
+
+document.getElementById("btn6").addEventListener("click", function() {
+    document.getElementById('section6').scrollIntoView({behavior: "smooth"})
+});
+
+document.getElementById("btn7").addEventListener("click", function() {
+    document.getElementById('section7').scrollIntoView({behavior: "smooth"})
+});
+
+document.getElementById("btn8").addEventListener("click", function() {
+    document.getElementById('section8').scrollIntoView({behavior: "smooth"})
+});
+
+document.getElementById("btn9").addEventListener("click", function() {
+    document.getElementById('section9').scrollIntoView({behavior: "smooth"})
+});
